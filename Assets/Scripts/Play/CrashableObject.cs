fe5c8c0ec9acffa2f6aade8f42d81d75170a970c
TbrtTkyk <103414431+TbrtTkyk@users.noreply.github.com>
2022-08-31 15:00:44 +0900

    破壊可能を示すComponentの追加、ついでにフォルダ整理

---
+++ Assets/Scripts/Play/CrashableObject.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 破壊可能なオブジェクトの動作を定義するComponent。
+[RequireComponent(typeof(Collider2D))]
+public class CrashableObject : MonoBehaviour
+{
+    [Tooltip("破壊されてから完全にオブジェクトが消えるまでの時間")]
+    [SerializeField] private float CrashDuration = 0f;
+
+    private Collider2D _collider;
+
+    void Awake()
+    {
+        _collider = GetComponent<Collider2D>();
+    }
+
+    // 破壊時に呼び出されるメソッド
+    [ContextMenu("Debug/Crash")]
+    private void Crash()
+    {
+        // 当たり判定を無効化
+        _collider.enabled = false;
+
+        // 指定秒数後に自身を破壊する（アニメーション用の待機時間）
+        Destroy(gameObject, CrashDuration);
+    }
+}
