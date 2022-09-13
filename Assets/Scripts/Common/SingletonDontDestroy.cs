e85a2f95058c9dbb51ac49a16264caf4488815bb
masatopia009 <masato.masaoka@gmail.com>
2022-09-13 09:43:56 +0900

    SingletonDontDestroyを追加

---
+++ Assets/Scripts/Common/SingletonDontDestroy.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class  SingletonDontDestroy<U> 
+            : SingletonMonoBehaviourInSceneBase<SingletonDontDestroy<U>> 
+    where U : SingletonMonoBehaviourInSceneBase<SingletonDontDestroy<U>>
+{
+    public static new U Instance { get; private set; }
+
+    // Start is called before the first frame update
+    protected override void Awake()
+    {
+        if (null != Instance && Instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+
+        Instance = this as U;
+
+        base.Awake();
+
+        DontDestroyOnLoad(this);
+    }
+}
