180ee0723a2f7f5684434aca61d89b98f9dc8caa
[taiga0412] <b2202410@photon.chitose.ac.jp>
2022-09-15 17:22:11 +0900

    ステージ未選択時の処理を追加

--- Assets/Scripts/StageSelect/LoadStage.cs
+++ Assets/Scripts/StageSelect/LoadStage.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using UnityEngine.UI;
 
 // NextButtonにアタッチ
 public class LoadStage : MonoBehaviour
@@ -13,16 +14,27 @@ public class LoadStage : MonoBehaviour
     private int stageNum = -1;
     private Stage stage = new Stage();
 
+    private Button button;
+
+    private void Start()
+    {
+        button = GetComponent<Button>();
+        button.interactable = false;
+    }
+
     public void SetStageNum(Stage stage)
     {
         this.stageNum = stage.StageNum;
         this.stage = stage;
+
+        button.interactable = true;
     }
 
     public void OnLoadStage()
     {
         Debug.Log($"Push loadStage : stageNum -> {stageNum}");
         Debug.Log("OnLoadStage : Debug用処理");
+
         string sceneName = scenePrefix + stageNum;
         StageSelectGlobal.Instance.Stage = stage;
         StageSelectGlobal.Instance.StageDataBase = stageDataBase;
